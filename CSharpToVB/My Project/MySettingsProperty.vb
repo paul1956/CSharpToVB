68b380887be0482b4cf4ba13d1afb1dc618169ae
Paul M Cohen <paul1956@users.noreply.github.com>
2019-11-27 15:26:57 -0800

    Separate MySettingsProperty from Settings.Designer. Add new Framework setting.

---
+++ CSharpToVB/My Project/MySettingsProperty.vb
@@ -0,0 +1,15 @@
+﻿Namespace My
+
+    <HideModuleName(),
+     DebuggerNonUserCode(),
+     Runtime.CompilerServices.CompilerGenerated()>
+    Friend Module MySettingsProperty
+
+        <ComponentModel.Design.HelpKeyword("My.Settings")>
+        Friend ReadOnly Property Settings() As MySettings
+            Get
+                Return MySettings.Default
+            End Get
+        End Property
+    End Module
+End Namespace
