8c8a469319927e4863d81125b29514dd9187a0f8
Paul M Cohen <paul1956@users.noreply.github.com>
2020-01-03 20:43:19 -0800

    Update AssemblyInfo.vb to use Winforms Design and move file.

---
+++ CSharpToVB/ExceptionUtils.vb
@@ -0,0 +1,13 @@
+﻿Namespace Microsoft.VisualBasic.CompilerServices
+    Public NotInheritable Class ExceptionUtils
+        ''' <summary>
+        ''' Returns a new instance of ArgumentNullException with message: "Argument cannot be Nothing."
+        ''' </summary>
+        ''' <param name="ArgumentName">The name of the argument (parameter). Not localized.</param>
+        ''' <returns>A new instance of ArgumentNullException.</returns>
+        Friend Shared Function GetArgumentNullException(ArgumentName As String) As ArgumentNullException
+            Return New ArgumentNullException(ArgumentName, "Argument cannot be Nothing.")
+        End Function
+
+    End Class
+End Namespace
