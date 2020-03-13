b361c70a9eed85fab5f0b14cd9da7d2db21a40c9
Paul M Cohen <paul1956@users.noreply.github.com>
2020-03-13 01:07:47 -0700

    Fix some disposable errors, update version and issue #3 GitHub should be git

--- CSharpToVB/Form1.vb
+++ CSharpToVB/Form1.vb
@@ -504,7 +504,9 @@ Public Class Form1
     End Sub
 
     Private Function Convert_Compile_Colorize(RequestToConvert As ConvertRequest, CSPreprocessorSymbols As List(Of String), VBPreprocessorSymbols As List(Of KeyValuePair(Of String, Object)), OptionalReferences() As MetadataReference, CancelToken As CancellationToken) As Boolean
-        _resultOfConversion = ConvertInputRequest(RequestToConvert, CSPreprocessorSymbols, VBPreprocessorSymbols, OptionalReferences:=OptionalReferences, AddressOf ReportException, mProgress:=New TextProgressBar(ConversionProgressBar), CancelToken:=CancelToken)
+        Dim mProgress As TextProgressBar = New TextProgressBar(ConversionProgressBar)
+        _resultOfConversion = ConvertInputRequest(RequestToConvert, CSPreprocessorSymbols, VBPreprocessorSymbols, OptionalReferences:=OptionalReferences, AddressOf ReportException, mProgress:=mProgress, CancelToken:=CancelToken)
+        mProgress.Dispose()
         mnuFileSaveAs.Enabled = Me._resultOfConversion.ResultStatus = ResultTriState.Success
         Select Case _resultOfConversion.ResultStatus
             Case ResultTriState.Success
--- CSharpToVB/TextProgressBar.vb
+++ CSharpToVB/TextProgressBar.vb
@@ -3,11 +3,16 @@ Imports System.Threading
 
 Public Class TextProgressBar
     Implements IProgress(Of ProgressReport)
+    Implements IDisposable
 
-    Private ReadOnly _defaultFont As Font = New Font("Segoe UI", 8, FontStyle.Bold)
+    Private ReadOnly _defaultFont As New Font("Segoe UI", 9, FontStyle.Bold)
     Private ReadOnly _progressBar As ToolStripProgressBar
 
     Public Sub New(Bar As ToolStripProgressBar)
+        If Bar Is Nothing Then
+            Throw New ArgumentNullException(NameOf(Bar))
+        End If
+
         _progressBar = Bar
         _progressBar.TextImageRelation = TextImageRelation.Overlay
         _progressBar.Value = 0
@@ -62,5 +67,37 @@ Public Class TextProgressBar
 
         pbPrecentage(_progressBar, $"{value.Current:N0} of {value.Maximum:N0}")
     End Sub
+
+#Region "IDisposable Support"
+    Private disposedValue As Boolean ' To detect redundant calls
+
+    ' IDisposable
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not disposedValue Then
+            If disposing Then
+                _defaultFont.Dispose()
+            End If
+
+            ' TODO: free unmanaged resources (unmanaged objects) and override Finalize() below.
+            ' TODO: set large fields to null.
+        End If
+        disposedValue = True
+    End Sub
+
+    ' TODO: override Finalize() only if Dispose(disposing As Boolean) above has code to free unmanaged resources.
+    'Protected Overrides Sub Finalize()
+    '    ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+    '    Dispose(False)
+    '    MyBase.Finalize()
+    'End Sub
+
+    ' This code added by Visual Basic to correctly implement the disposable pattern.
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+        Dispose(True)
+        ' TODO: uncomment the following line if Finalize() is overridden above.
+        GC.SuppressFinalize(Me)
+    End Sub
+#End Region
 
 End Class
