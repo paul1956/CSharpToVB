221d375ad80ce3f5446c093b3f7ff6f3545888b1
Paul M Cohen <paul1956@users.noreply.github.com>
2020-05-22 20:23:19 -0700

    Make ConfigAwait explicit in tests, remove suppression file.

--- CSharpToVB.Tests/GlobalSuppressions.vb
+++
@@ -1,7 +0,0 @@
-﻿' Licensed to the .NET Foundation under one or more agreements.
-' The .NET Foundation licenses this file to you under the MIT license.
-' See the LICENSE file in the project root for more information.
-
-Imports System.Diagnostics.CodeAnalysis
-
-<Assembly: SuppressMessage("Reliability", "CA2007:Consider calling ConfigureAwait on the awaited task", Justification:="Only for library code", Scope:="module")>
