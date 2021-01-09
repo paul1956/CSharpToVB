c17f787a965d0dc8b470d6a9b20e08036b389b86
Paul M Cohen <paul1956@users.noreply.github.com>
2021-01-08 21:40:35 -0800

    Remove code to deal with disabled control Windows handles it.

--- CSharpToVB/ThemeSupport/ThemeSupport.vb
+++ CSharpToVB/ThemeSupport/ThemeSupport.vb
@@ -9,61 +9,54 @@ Module ThemeSupport
             Dim isInToolStrip As Boolean = TypeOf component.Parent Is ToolStrip
             If TypeOf component Is Panel Then
                 ChangeTheme(scheme, component.Controls)
-                CheckAndSetColor(component, scheme, "PanelBoarderStyle", isInToolStrip, enabled)
+                CheckAndSetColor(component, scheme, "PanelBoarderStyle", isInToolStrip)
             ElseIf TypeOf component Is MenuStrip Then
                 ChangeTheme(scheme, component.Controls)
-                CheckAndSetColor(component, scheme, "MenuStrip", isInToolStrip, enabled)
+                CheckAndSetColor(component, scheme, "MenuStrip", isInToolStrip)
             ElseIf TypeOf component Is SplitContainer Then
                 ChangeTheme(scheme, component.Controls)
-                CheckAndSetColor(component, scheme, "SplitContainer", isInToolStrip, enabled)
+                CheckAndSetColor(component, scheme, "SplitContainer", isInToolStrip)
             ElseIf TypeOf component Is StatusStrip Then
                 ChangeTheme(scheme, component.Controls)
-                CheckAndSetColor(component, scheme, "StatusBar", isInToolStrip, enabled)
+                CheckAndSetColor(component, scheme, "StatusBar", isInToolStrip)
             ElseIf TypeOf component Is ToolStrip Then
                 ChangeTheme(scheme, component.Controls)
-                CheckAndSetColor(component, scheme, "Toolbar", isInToolStrip, enabled)
+                CheckAndSetColor(component, scheme, "Toolbar", isInToolStrip)
             ElseIf TypeOf component Is ContextMenuStrip Then
                 ChangeTheme(scheme, component.Controls)
-                CheckAndSetColor(component, scheme, "FloatingMenu", isInToolStrip, enabled)
+                CheckAndSetColor(component, scheme, "FloatingMenu", isInToolStrip)
             ElseIf TypeOf component Is Button Then
-                CheckAndSetColor(component, scheme, "Button", isInToolStrip, enabled)
+                CheckAndSetColor(component, scheme, "Button", isInToolStrip)
             ElseIf TypeOf component Is CheckBox Then
-                CheckAndSetColor(component, scheme, "CheckBox", isInToolStrip, enabled)
+                CheckAndSetColor(component, scheme, "CheckBox", isInToolStrip)
             ElseIf TypeOf component Is ComboBox Then
-                CheckAndSetColor(component, scheme, "ComboBox", isInToolStrip, enabled)
+                CheckAndSetColor(component, scheme, "ComboBox", isInToolStrip)
             ElseIf TypeOf component Is Label Then
-                CheckAndSetColor(component, scheme, "Label", isInToolStrip, enabled)
+                CheckAndSetColor(component, scheme, "Label", isInToolStrip)
             ElseIf TypeOf component Is ListBox Then
-                CheckAndSetColor(component, scheme, "ListBox", isInToolStrip, enabled)
+                CheckAndSetColor(component, scheme, "ListBox", isInToolStrip)
             ElseIf TypeOf component Is ProgressBar Then
-                CheckAndSetColor(component, scheme, "ProgressBar", isInToolStrip, enabled)
+                CheckAndSetColor(component, scheme, "ProgressBar", isInToolStrip)
             ElseIf TypeOf component Is LineNumbersForRichTextBox Then
-                CheckAndSetColor(component, scheme, "LineNumbersForRichTextBox", isInToolStrip, enabled)
+                CheckAndSetColor(component, scheme, "LineNumbersForRichTextBox", isInToolStrip)
             ElseIf TypeOf component Is RichTextBox Then
-                CheckAndSetColor(component, scheme, "RichTextBox", isInToolStrip, enabled)
+                CheckAndSetColor(component, scheme, "RichTextBox", isInToolStrip)
             Else
                 Stop
             End If
         Next
     End Sub
 
-    Sub CheckAndSetColor(cont As Control, scheme As Dictionary(Of String, (ForeGround As Color, Background As Color)), ControlName As String, IsInToolStrip As Boolean, Enabled As Boolean)
-        If Not Enabled Then
-            ControlName = $"{ControlName}Disabled"
-        End If
+    Sub CheckAndSetColor(cont As Control, scheme As Dictionary(Of String, (ForeGround As Color, Background As Color)), ControlName As String, IsInToolStrip As Boolean)
         If IsInToolStrip Then
             ControlName = $"ToolStrip{ControlName}"
         End If
         Dim colors As (Foreground As Color, Background As Color) = scheme(ControlName)
 
-        ' For light mode you should never hit a stop (when enabled) but you do
-        ' Need to investigate
         If cont.BackColor <> colors.Background Then
-            'Stop
             cont.BackColor = colors.Background
         End If
         If cont.ForeColor <> colors.Foreground Then
-            'Stop
             cont.ForeColor = colors.Foreground
         End If
     End Sub
