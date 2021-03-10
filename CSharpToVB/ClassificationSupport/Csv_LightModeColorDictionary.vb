d6c000772a8b2f28ee188da99511c5612f6b7975
Paul M Cohen <paul1956@users.noreply.github.com>
2021-03-09 23:46:59 -0800

    Add missing header on Color Selector Dictionary files

--- CSharpToVB/ClassificationSupport/Csv_LightModeColorDictionary.vb
+++ CSharpToVB/ClassificationSupport/Csv_LightModeColorDictionary.vb
@@ -1,4 +1,8 @@
-﻿Partial Public Module ColorSelector
+﻿' Licensed to the .NET Foundation under one or more agreements.
+' The .NET Foundation licenses this file to you under the MIT license.
+' See the LICENSE file in the project root for more information.
+
+Partial Public Module ColorSelector
     Friend _lightModeColorDictionary As New Dictionary(Of String, ColorDescriptor)(StringComparer.OrdinalIgnoreCase) From {
         {ThemeDefaultColor, New ColorDescriptor(Color.FromARGB(0, 0, 0), Color.FromARGB(255, 255, 255))},
         {ClassName, New ColorDescriptor(Color.FromARGB(0, 128, 128), Color.FromARGB(255, 255, 255))},
